ba719bcb39aca86a987c15ccb844264965cb6877
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2022-05-23 17:14:06 +0900

    Create number_baseball.cpp

---
+++ number_baseball.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define Weight 0.05
+
+int main(){
+	
+	typedef struct _baseNum{
+		int ID;
+		int calledNum;
+		float percentage;
+	}baseNum;
+	
+	baseNum baseArr[4][10];
+	
+	return 0;
+}
