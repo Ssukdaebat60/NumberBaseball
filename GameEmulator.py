cfe5b24a1fad29ca5818d3d840f8f43f625adccb
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2022-05-29 22:31:12 +0900

    Update GameEmulator.py
    Modified SelectedBandS Function
    Have to Modify selected Functions.(no row & col)
    Have toTest compare Function If Len(SList)==4 is Operatable
    Have to Add Function of Increasing Percentage

--- GameEmulator.py
+++ GameEmulator.py
@@ -4,24 +4,29 @@ class GameEmulator:
       self.CList = CList
   
   def baseNumChoice(self):
-    return [max(PList[i]) for i in range(4)]
+    return [max(self.PList[i]) for i in range(4)]
   
   def selectedS(self, row, col, num):
-    CList[row][col] += 1
-    PList[row][col] += ((CList[row][col]-1)*PList[row][col]+(num/4))/CList[row][col]
+    self.CList[row][col] += 1
+    self.PList[row][col] += ((self.CList[row][col]-1)*self.PList[row][col]+(num/4))/self.CList[row][col]
     
   def selectedB(self, row, col, num):
       for i in range(4):
           if i == row:
               continue
-          CList[i][col] += 1
-          PList[i][col] += ((CList[i][col]-1)*PList[i][col]+(num/4))/CList[i][col]
+          self.CList[i][col] += 1
+          self.PList[i][col] += ((self.CList[i][col]-1)*self.PList[i][col]+(num/4))/self.CList[i][col]
   
-  def selectedBandS(self, row, col, W):
-    PList[row][col] += num*W
+  def selectedBandS(self, B, S):
+    for i in range(4):
+        if self.PList[i].count(0) == 9:
+            S-=1
+    if S == 0:
+        selectedB(1,1,1)
+    self.PList[row][col] += num*W
 
   def delete(self, row, col):
-    PList[row][col] = 0
+    self.PList[row][col] = 0
     
   def compare(self, SList, AList):
     return len([SList.pop( i - (8 - len(SList) ) ) for i in range(4) if SList[ i - (8 - len(SList)) ] == AList[i]]) + 10*len(set.intersection(set(AList), set(SList)))
